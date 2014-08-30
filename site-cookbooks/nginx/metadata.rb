a55ff66f098463bac766c10a2ced7b679bc51c38
Daniel Ku <kjunine@gmail.com>
2014-08-30 12:22:06 +0900

    Adds nginx cookbook.

---
+++ site-cookbooks/nginx/metadata.rb
@@ -0,0 +1,7 @@
+name             'nginx'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'

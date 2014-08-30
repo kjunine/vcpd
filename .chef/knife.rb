2bb5b09540425b9fcfe0d6ac356c1939e1406186
Daniel Ku <kjunine@gmail.com>
2014-08-30 12:07:13 +0900

    Adds cookbook configs.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,7 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+
+cookbook_copyright       'Daniel Ku'
+cookbook_email           'kjunine@gmail.com'
+cookbook_license         'mit'
